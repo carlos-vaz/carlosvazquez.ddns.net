6a52f982584d23769ee30a81fb7331ba47fe327d
Carlos Vazquez <crlsvzqz5@gmail.com>
2019-05-27 19:12:43 +0100

    webusers

--- html/init_session.php
+++ html/init_session.php
@@ -1,4 +1,7 @@
 <?php
 // Start a bash script implementing a simple Read Eval Print loop
 shell_exec("nohup /var/www/html/loop.sh > /dev/null &");
+
+// Wait for initialization message from loop.sh
+//shell_exec("cat ");
 ?>
--- html/loop.sh
+++ html/loop.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+# Invoked automatically after loading carlosvazquez.ddns.net
 
-#while true
-#do
-if read line </var/www/html/fifo/fifo_tobash; then
-	if [[ "$line" == 'q' ]]; then
-		break
+# First, asign a unique user ID and store in ../security/userlog
+uid="webuser-"$(wc -l ../security/userlog | awk -F " " '{print $1}')
+echo $uid >> ../security/userlog
+
+# Create named pipes, unique to this user, for passing commands to bash and outputs to php
+mkfifo ./fifo/${uid}_tobash
+mkfifo ./fifo/${uid}_tophp
+
+# Send welcome messgae to init_session.php, who is waiting
+
+
+while true
+do
+	if read line </var/www/html/fifo/${uid}_tobash; then
+		if [[ "$line" == 'q' ]]; then
+			break
+		fi
+		echo "Read: " $line > /var/www/html/fifo/${uid}_tophp
 	fi
-	echo "Read: " $line > /var/www/html/fifo/fifo_tophp
-fi
-#done
-#echo "Finished" > /var/www/html/fifo/fifo_tophp
+done
+echo "Finished" > /var/www/html/fifo/${uid}_tophp
