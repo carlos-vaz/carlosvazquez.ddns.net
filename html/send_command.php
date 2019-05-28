4dc4e35214ffdab8ce3df450da1786de2a992c2d
crlsvzqz5 <32377707+crlsvzqz5@users.noreply.github.com>
2019-05-28 11:13:16 -0400

    cleaner 2

--- html/send_command.php
+++ html/send_command.php
@@ -11,6 +11,7 @@ if (!file_exists("/var/www/html/fifo/" . $requested_session . "_tophp") || !file
 
 // Pass command request to session
 shell_exec("echo " . $command . " > /var/www/html/fifo/" . $requested_session . "_tobash"); // blocks php until/unless session.sh has begun
+
 // send output of bash back to the client
 exec("cat /var/www/html/fifo/" . $requested_session . "_tophp", $OUTPUT);
 foreach($OUTPUT as $line) {
