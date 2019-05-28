679e9ecc52901e8688e1fa5f29cdf65dadb43f78
Carlos Vazquez <crlsvzqz5@gmail.com>
2019-05-28 22:51:47 +0100

    jail session

--- html/send_command.php
+++ html/send_command.php
@@ -5,15 +5,15 @@ $command = $_POST["cmd"];
 //$command = "harmless";
 
 // Verify requested sessionid
-if (!file_exists("/var/www/fifo/" . $requested_session . "_tophp") || !file_exists("/var/www/fifo/" . $requested_session . "_tobash")) {
+if (!file_exists("/var/www/jail/fifo/" . $requested_session . "_tophp") || !file_exists("/var/www/jail/fifo/" . $requested_session . "_tobash")) {
 	exit("The session you are trying to reach has expired! Please reload the page. \n");
 }
 
 // Pass command request to session
-shell_exec("echo " . $command . " > /var/www/fifo/" . $requested_session . "_tobash"); // blocks php until/unless session.sh has begun
+shell_exec("echo " . $command . " > /var/www/jail/fifo/" . $requested_session . "_tobash"); // blocks php until/unless session.sh has begun
 
 // send output of bash back to the client
-exec("cat /var/www/fifo/" . $requested_session . "_tophp", $OUTPUT);
+exec("cat /var/www/jail/fifo/" . $requested_session . "_tophp", $OUTPUT);
 foreach($OUTPUT as $line) {
 	echo $line . "<br>";
 }
--- scripts/start_session_in_jail.c
+++ scripts/start_session_in_jail.c
@@ -3,10 +3,13 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	setuid(0); // Not actually needed, since SUID permission bit is set for change_root binary
-	char chroot_command[256];
-	snprintf(chroot_command, sizeof(chroot_command), "%s%s", "chroot /var/www/jail /session.sh ", argv[1]);
-	system(chroot_command);
+        // Not actually needed, since SUID permission bit is set for start_session_in_jail binary
+        setuid(0);
+        char chroot_command[256];
+        // argv[1] contains the session ID
+        snprintf(chroot_command, sizeof(chroot_command), "%s%s", "chroot /var/www/jail /session.sh ", argv[1]);
+        system(chroot_command);
 
-	return 0;
+        return 0;
 }
+
