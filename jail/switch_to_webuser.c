9d151689bf78a93a4706d422b37826683efb4c25
crlsvzqz5 <32377707+crlsvzqz5@users.noreply.github.com>
2019-05-28 21:30:55 -0400

    comments

--- jail/switch_to_webuser.c
+++ jail/switch_to_webuser.c
@@ -2,11 +2,14 @@
 #include <unistd.h>
 #include <stdio.h>
 
+// Performs downgrading of privilege to user "webuser", and starts a bash session
+// argv[1] contains the session ID  
 int main(int argc, char *argv[]) {
         setuid(1001); // uid of webuser
         char start_session[256];
-        // argv[1] contains the session ID
         snprintf(start_session, sizeof(start_session), "%s%s", "/bin/bash ./session.sh ", argv[1]);
+        
+        // Won't return
         system(start_session);
 
         return 0;
