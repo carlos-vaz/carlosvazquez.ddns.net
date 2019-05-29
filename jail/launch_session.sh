4fb9d3fe2f0d73920158aeb5569b3c32c5dc8abd
crlsvzqz5 <32377707+crlsvzqz5@users.noreply.github.com>
2019-05-29 00:22:29 -0400

    comments

--- jail/launch_session.sh
+++ jail/launch_session.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Invoked automatically (by init_session.php --> enter_jail.sh --> here) after loading
 # carlosvazquez.ddns.net. By the time this script is called, the new root / is actually
-# /var/www/jail/ and all commands are run with root permission. This downgrades the 
-# permission by setting the UID to 1001 (webuser)
+# /var/www/jail/ and all commands are run with root permission. This scipt then downgrades the 
+# permission back by setting the UID to 1001 (webuser)
 
 sessionid=$1;
 
